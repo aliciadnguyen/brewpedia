befe907e69106a7f85ebd7755f84caa5c4d4a17d
Alicia Nguyen <aliciadnguyen@gmail.com>
2016-03-21 18:07:13 -0700

    added gem rspec and created controller spec test

---
+++ spec/controllers/beers_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe BeersController, :type => :controller do 
+	describe "GET #index" do 
+		it "responds successfully with HTTP 200 status code" do 
+			get :index
+			expect(response).to be_success
+			expect(response).to have_http_status(200)
+		end
+
+		it "renders the index template" do 
+			get :index
+			expect(response).to render_template("index")
+		end
+
+		it "populates all the beer types" do 
+			beer1, beer2 = Beer.create!, Beer.create!
+			get :index
+			expect(assigns(:beers)).to match_array([beer1, beer2])
+		end
+	end
+end
